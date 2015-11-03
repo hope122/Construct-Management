0f2a19e79f61d0178a9b013c2428685ec2159e36
hope122 <hope080122@gmail.com>
2015-11-03 15:14:52 +0800

    20131103
    Material

---
+++ module/Application/src/Application/Controller/MaterialController.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use System_APService\clsSystem;
+
+class MaterialController extends AbstractActionController
+{
+	//不執行任何動作
+	public function indexAction()
+    {
+		$this->viewContnet['pageContent'] = 'Please Select Your Action and Try Again!';
+        return new ViewModel($this->viewContnet);
+    }
+	//取得選單
+    public function ApplicationAction()
+    {
+        //session_start();
+		$VTs = new clsSystem;
+		$VTs->initialization();
+		
+		//-----BI開始-----  Application材料申請頁面
+        
+                $mpath=dirname(__DIR__) . "\\..\\..\\..\\..\\public\\include\\pageSetting\\styles\\material\\application.html";
+        
+                $html=$VTs->GetHtmlContent($mpath);
+                $d_type_a = $VTs->json2data($VTs->UrlDataGet("http://127.0.0.1:88/material/getdbdata?type=su_supply"));
+                $str='';
+                foreach($d_type_a as $opData) {
+                    $str.='<option value='.$opData->uid.'>'.$opData->name.'</option>';
+                    }
+                $html=str_replace('@@opt_supply@@',$str,$html);
+                $ls_petition= $VTs->json2data($VTs->UrlDataGet("http://127.0.0.1:88/material/getdbdata?type=el_petition"));
+        
+                if($ls_petition==null){
+                    $ls='無資料';
+                }else{
+                    $ls_path=dirname(__DIR__) . "\\..\\..\\..\\..\\public\\include\\pageSetting\\styles\\material\\list.html";
+                    $tr_path=dirname(__DIR__) . "\\..\\..\\..\\..\\public\\include\\pageSetting\\styles\\material\\tr.html";
+                    $ls=$VTs->GetHtmlContent($ls_path);
+                    $tr=$VTs->GetHtmlContent($tr_path);
+                    $trstr='';
+//                    print_r($ls_petition);
+                    foreach($ls_petition as  $id => $lsData) {
+                        $trs=$tr;
+                        $trs=str_replace('@@id@@',$id+1,$trs);
+                        $trs=str_replace('@@supply@@',$lsData->su_name,$trs);
+                        $trs=str_replace('@@name@@',$lsData->ma_name,$trs);
+                        $trs=str_replace('@@intime@@',$lsData->date,$trs);
+                        $trs=str_replace('@@place@@',$lsData->place,$trs);
+                        $trs=str_replace('@@count@@',$lsData->count,$trs);
+                        if($lsData->check==1){
+                            $str_order='已確認';
+                        }else{
+                            $str_order="<input type='checkbox' class='cls_order' value=".$lsData->uid.">";
+                        }
+                        $trs=str_replace('@@order@@',$str_order,$trs);
+                        $trs=str_replace('@@uid@@',$lsData->uid,$trs);
+                        $trstr.=$trs;
+                    }
+                    $ls=str_replace('@@tr@@',$trstr,$ls);
+                    
+                }
+                $html=str_replace('@@list@@',$ls,$html);
+        
+                $pageContent=$html;
+        //-----BI結束-----
+         //關閉資料庫連線
+        $VTs->DBClose();
+        //釋放
+		$VTs = null;
+		$this->viewContnet['pageContent'] = $pageContent;
+        return new ViewModel($this->viewContnet);
+    }
+   public function getprjuidAction()
+    {
+        //session_start();
+		$VTs = new clsSystem;
+		$VTs->initialization();
+		
+		//-----BI開始-----  get prjuid 傳入廠商ＩＤ 回傳品項html option內容
+            //取得廠商ID
+            $suid=$_GET['suid'];
+            //廠商id傳入ap 取得品項陣列
+            $arr_prj_material = $VTs->json2data($VTs->UrlDataGet("http://127.0.0.1:88/material/getdbdata?type=prj_materiel&suid=".$suid));
+            print_r($arr_prj_material);
+      
+            //陣列組成html
+            $html="<option value=0>請選擇</option>";
+            foreach($arr_prj_material as $prj){
+                $html.="<option value=".$prj->uid.">".$prj->name."</option>";
+            }
+            //印出html
+            $pageContent=$html;
+        //-----BI結束-----
+         //關閉資料庫連線
+        $VTs->DBClose();
+        //釋放
+		$VTs = null;
+		$this->viewContnet['pageContent'] = $pageContent;
+        return new ViewModel($this->viewContnet);
+    }
+
+}
---
+++ public/include/js/page/material.js
@@ -0,0 +1,127 @@
+var apurl='http://127.0.0.1:88/'
+var pageurl='http://127.0.0.1:168/'
+$(function(){
+    $("#inp_prjuid").change(function() {
+        var uid=$(this).val();
+
+        if(uid!=0){
+            $.get(apurl+'material/getdbdata',{ type: "qu_materiel",uid:uid},function(data){
+                
+                var o=jQuery.parseJSON(data);
+               
+                $('#inp_total').val((o[0].count==null)?0:o[0].count);
+                $('#inp_finished').val((o[0].count_in==null)?0:o[0].count_in);
+                $('#inp_prjuid').val((o[0]. uid==null)?0:o[0].uid);
+            });
+        }else{
+             $('#inp_total').val(0);
+             $('#inp_finished').val(0);
+             $('#inp_prjuid').val(0);
+        }
+    });
+    $("#inp_supply").change(function() {
+        var suid=$(this).val();
+         $.get(pageurl+'material/getprjuid',{ suid: suid},function(data){
+                $("#inp_prjuid").empty();
+                $("#inp_prjuid").append(data);
+            });
+        });
+
+});
+
+function chkinp(){
+    var chk=true;
+    var d='';
+    d += 'type=el_petition&';
+//    var d='{type:el_petition,';
+    $( ".cls_inp" ).each(function( index ) {
+        if($(this).val()==0 || $(this).val()==''){
+            chk=false;
+        }else{
+            d += $(this).prop('id')+'='+$(this).val()+'&';
+            
+//            d+=$(this).prop('id')+':'+$(this).val()+';';
+        }
+       
+    });
+    d = d.substring(0,d.length-1);
+//    console.log(d);
+    if(!chk){
+        alert('資料不完整！');
+        }else{
+            $.ajax({
+               url: apurl+'material/dbinsert',
+               type: "POST",
+               data: d,
+               async:       false,
+               //dataType: "JSON",
+               success: function(rs){
+//                console.log(rs);
+                    alert('新增成功！');
+                      location.reload();
+               },
+               error: function(e){
+                    alert('儲存失敗！');
+               }
+            });
+    }
+   
+}
+
+function chk_order(){
+    var d='';
+    var str='';
+    var arr = [];
+    d += 'type=chkorder&id=';
+    $( ".cls_order" ).each(function( index ) {
+//                console.log($(this).val());
+        if(this.checked){
+            d += $(this).val()+',';
+//            console.log($('#tr'+index).find('.name').html()+"  "+$('#tr'+index).find('.time').html()+"");
+            index++;
+//            console.log('#tr'+index);
+            str+=$('#tr'+index).find('.supply').html()+"  "+$('#tr'+index).find('.name').html()+"  "+$('#tr'+index).find('.time').html()+"  "+$('#tr'+index).find('.count').html()+"\n";
+            arr.push({ type: 1, dataid: $(this).val(),date:$('#tr'+index).find('.time').html() });
+            
+        }
+    });
+//    console.log(arr);
+    d = d.substring(0,d.length-1);
+    if(!str==''){
+        r=confirm('確認資料：\n'+str);
+        if(r){
+               $.ajax({
+                   url: apurl+'material/dbmodify',
+                   type: "POST",
+                   data: d,
+                   async:false,
+                   //dataType: "JSON",
+                   success: function(rs){
+//                    console.log(rs);
+                        send_qclist(arr);
+                        alert('送出訂單！');
+                        location.reload();
+                   },
+                   error: function(e){
+                        alert('儲存失敗！');
+                   }
+                });
+        }else{
+            location.reload();
+        }
+    }
+}
+
+function send_qclist(arr){
+     $.ajax({
+       url: apurl+'qc/dbinsert',
+       type: "POST",
+       data: {type:"qc_checklist",arr:arr},
+       async:false,
+       //dataType: "JSON",
+       success: function(rs){
+                console.log(rs);
+       }
+    });
+
+}
