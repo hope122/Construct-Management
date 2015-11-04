2c610cd63cdf1ebbcfa30602808469f2cef7f0a1
hope122 <hope080122@gmail.com>
2015-11-04 15:54:41 +0800

    aprul 修改

--- public/include/js/config/config.js
+++ public/include/js/config/config.js
@@ -1,5 +1,17 @@
+var apurl="http://211.21.170.18:99";
+//var apurl="http://127.0.0.1:88";
 var configObject = {
-    "LoginUrl": "http://127.0.0.1:99/login",
+    "LoginUrl": "http://211.21.170.18:99/login",
+    "langSet":"http://211.21.170.18:99/lang/page",
     "processLoginUrl": "menter/setlogin",
-    "getmenu": "http://127.0.0.1:99/pageaction/getmenu",
+    "getmenu": "http://211.21.170.18:99/pageaction/getmenu",
+    "menuProcess": "pageaction/menuprocess",
+    "getPosition": "pageaction/acposition",
+    "MaterialInsert":apurl+"/material/dbinsert",
+    "MaterialModify":apurl+"/material/dbmodify",
+    "MaterialDelete":apurl+"/material/dbdelete",
+    "MaterialGetData":apurl+"/material/getdbdata",
+    "QCInsert":apurl+"/qc/dbinsert",
+    "QCModify":apurl+"/qc/dbmodify",
+    "QCDelete":apurl+"/qc/dbdelete",
 };
