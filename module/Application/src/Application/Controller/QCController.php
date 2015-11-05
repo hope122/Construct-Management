8a540cd35e71fb25cb9eb51414db9bd0778ee6c8
hope122 <hope080122@gmail.com>
2015-11-05 23:08:05 +0800

    2015-11-05

--- module/Application/src/Application/Controller/QCController.php
+++ module/Application/src/Application/Controller/QCController.php
@@ -22,136 +22,33 @@ class QCController extends AbstractActionController
 		$VTs = new clsSystem;
 		$VTs->initialization();
 		
-		//-----BI開始-----  Application材料申請頁面
-                $mpath=dirname(__DIR__) . "\\..\\..\\..\\..\\public\\include\\pageSetting\\styles\\material\\application.html";
-                $html=$VTs->GetHtmlContent($mpath);
-                $d_type_a = $VTs->json2data($VTs->UrlDataGet("http://127.0.0.1:88/material/getdbdata?type=su_supply"));
-                $str='';
-                foreach($d_type_a as $opData) {
-                    $str.='<option value='.$opData->uid.'>'.$opData->name.'</option>';
-                    }
-                $html=str_replace('@@opt_supply@@',$str,$html);
-                $ls_petition= $VTs->json2data($VTs->UrlDataGet("http://127.0.0.1:88/material/getdbdata?type=el_petition"));
+		//-----BI開始-----  index QC審查首頁
+        //    $apurl='http://211.21.170.18:99';
+        $apurl='http://127.0.0.1:88';
+        $mpath=dirname(__DIR__) . "\\..\\..\\..\\..\\public\\include\\pageSetting\\styles\\qc\\index.html";
+        $trpath=dirname(__DIR__) . "\\..\\..\\..\\..\\public\\include\\pageSetting\\styles\\qc\\tr.html";
+        $html=$VTs->GetHtmlContent($mpath);
+        $tr=$VTs->GetHtmlContent($trpath);
+ 
+        $arr_data = $VTs->json2data($VTs->UrlDataGet($apurl."/qc/getdbdata"));
         
-                if($ls_petition==null){
-                    $ls='無資料';
-                }else{
-                    $ls_path=dirname(__DIR__) . "\\..\\..\\..\\..\\public\\include\\pageSetting\\styles\\material\\list.html";
-                    $tr_path=dirname(__DIR__) . "\\..\\..\\..\\..\\public\\include\\pageSetting\\styles\\material\\tr.html";
-                    $ls=$VTs->GetHtmlContent($ls_path);
-                    $tr=$VTs->GetHtmlContent($tr_path);
-                    
-                    $trstr='';
-//                    print_r($ls_petition);
-                    foreach($ls_petition as  $id => $lsData) {
-                        $trs=$tr;
-                        $trs=str_replace('@@id@@',$id+1,$trs);
-                        $trs=str_replace('@@supply@@',$lsData->su_name,$trs);
-                        $trs=str_replace('@@name@@',$lsData->ma_name,$trs);
-                        $trs=str_replace('@@intime@@',$lsData->date,$trs);
-                        $trs=str_replace('@@place@@',$lsData->place,$trs);
-                        if($lsData->check==1){
-                            $str_order='已確認';
-                        }else{
-                            $str_order="<input type='checkbox' class='cls_order' value=".$lsData->uid.">";
-                        }
-                        $trs=str_replace('@@order@@',$str_order,$trs);
-                        $trs=str_replace('@@uid@@',$lsData->uid,$trs);
-                        $trstr.=$trs;
-                    }
-                    $ls=str_replace('@@tr@@',$trstr,$ls);
-                    
-                }
-                $html=str_replace('@@list@@',$ls,$html);
+//        print_r($arr_data);
+        $str='';
+        foreach($arr_data as $index=>$data) {
+            $trs=$tr;
+            $trs=str_replace('@@id@@',$index+1,$trs);
+            $trs=str_replace('@@chkdata@@',$data->typename,$trs);
+            $trs=str_replace('@@date@@',$data->date,$trs);
+            $trs=str_replace('@@datec@@',$data->datec,$trs);
+            $trs=str_replace('@@isok@@',$data->isok,$trs);
+            $trs=str_replace('@@img@@',$data->imgid,$trs);
+            $trs=str_replace('@@remark@@',$data->remark,$trs);
+            $str.=$trs;
+        }
+        $html=str_replace('@@tr@@',$str,$html);
         
+
                 $pageContent=$html;
-        //-----BI結束-----
-         //關閉資料庫連線
-        $VTs->DBClose();
-        //釋放
-		$VTs = null;
-		$this->viewContnet['pageContent'] = $pageContent;
-        return new ViewModel($this->viewContnet);
-    }
-	//取得選單
-    public function ApplicationAction()
-    {
-        //session_start();
-		$VTs = new clsSystem;
-		$VTs->initialization();
-		
-		//-----BI開始-----  Application材料申請頁面
-                $mpath=dirname(__DIR__) . "\\..\\..\\..\\..\\public\\include\\pageSetting\\styles\\material\\application.html";
-        
-                $html=$VTs->GetHtmlContent($mpath);
-                $d_type_a = $VTs->json2data($VTs->UrlDataGet("http://127.0.0.1:88/material/getdbdata?type=su_supply"));
-                $str='';
-                foreach($d_type_a as $opData) {
-                    $str.='<option value='.$opData->uid.'>'.$opData->name.'</option>';
-                    }
-                $html=str_replace('@@opt_supply@@',$str,$html);
-                $ls_petition= $VTs->json2data($VTs->UrlDataGet("http://127.0.0.1:88/material/getdbdata?type=el_petition"));
-        
-                if($ls_petition==null){
-                    $ls='無資料';
-                }else{
-                    $ls_path=dirname(__DIR__) . "\\..\\..\\..\\..\\public\\include\\pageSetting\\styles\\material\\list.html";
-                    $tr_path=dirname(__DIR__) . "\\..\\..\\..\\..\\public\\include\\pageSetting\\styles\\material\\tr.html";
-                    $ls=$VTs->GetHtmlContent($ls_path);
-                    $tr=$VTs->GetHtmlContent($tr_path);
-                    
-                    $trstr='';
-//                    print_r($ls_petition);
-                    foreach($ls_petition as  $id => $lsData) {
-                        $trs=$tr;
-                        $trs=str_replace('@@id@@',$id+1,$trs);
-                        $trs=str_replace('@@supply@@',$lsData->su_name,$trs);
-                        $trs=str_replace('@@name@@',$lsData->ma_name,$trs);
-                        $trs=str_replace('@@intime@@',$lsData->date,$trs);
-                        $trs=str_replace('@@place@@',$lsData->place,$trs);
-                        if($lsData->check==1){
-                            $str_order='已確認';
-                        }else{
-                            $str_order="<input type='checkbox' class='cls_order' value=".$lsData->uid.">";
-                        }
-                        $trs=str_replace('@@order@@',$str_order,$trs);
-                        $trs=str_replace('@@uid@@',$lsData->uid,$trs);
-                        $trstr.=$trs;
-                    }
-                    $ls=str_replace('@@tr@@',$trstr,$ls);
-                    
-                }
-                $html=str_replace('@@list@@',$ls,$html);
-        
-                $pageContent=$html;
-        //-----BI結束-----
-         //關閉資料庫連線
-        $VTs->DBClose();
-        //釋放
-		$VTs = null;
-		$this->viewContnet['pageContent'] = $pageContent;
-        return new ViewModel($this->viewContnet);
-    }
-   public function getprjuidAction()
-    {
-        //session_start();
-		$VTs = new clsSystem;
-		$VTs->initialization();
-		
-		//-----BI開始-----  get prjuid 傳入廠商ＩＤ 回傳品項html option內容
-            //取得廠商ID
-            $suid=$_GET['suid'];
-            //廠商id傳入ap 取得品項陣列
-            $arr_prj_material = $VTs->json2data($VTs->UrlDataGet("http://127.0.0.1:88/material/getdbdata?type=prj_materiel&suid=".$suid));
-            print_r($arr_prj_material);
-      
-            //陣列組成html
-            $html="<option value=0>請選擇</option>";
-            foreach($arr_prj_material as $prj){
-                $html.="<option value=".$prj->uid.">".$prj->name."</option>";
-            }
-            //印出html
-            $pageContent=$html;
         //-----BI結束-----
          //關閉資料庫連線
         $VTs->DBClose();
