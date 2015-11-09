02f7b05460d84f7d83c1d88505ffd20d3fc2902b
DelphiRace <delphi.race@gmail.com>
2015-11-09 11:59:48 +0800

    update

---
+++ public/include/js/socket/conn.js
@@ -0,0 +1,19 @@
+var socket = io.connect('http://211.21.170.18:7077');
+var systemID = '';
+
+socket.on('chatMsg', function(data) {
+    if(data.systemID != systemID){
+        showNoticeToast(data.userName+':<br/>'+data.msg);
+    }
+});
+
+socket.on('conn', function (data) {
+
+    var postdata = {
+        'uid': loginArr["uuid"],
+        'userName' : loginArr["loginName"]
+    }
+    systemID = data.systemID;
+    socket.emit('login', postdata,function(result){});
+
+});
