795dae25e07508b53d271ac52264a7799272d86a
hope122 <hope080122@gmail.com>
2015-11-17 14:24:25 +0800

    config

--- public/include/js/page/qc/photolist.js
+++ public/include/js/page/qc/photolist.js
@@ -1,6 +1,17 @@
 
 $(function(){
-$.post(configObject.QCGetData+"?type=qc_checklist",function(result){
-        console.log(JSON.parse(result));
-  });
+	var json;
+$.get(configObject.QCGetData+"?type=qc_checklist",function(result){
+	json=JSON.parse(result);
+	for (var key in json) {
+	    if (!json[key].imgid===null) {
+	    	console.log(json[key].uid);
+	    };
+	}
+
+ });
+
+// $.post("http://211.21.170.18:99/pageaction/getqcimglist",{qcid:5},function(result){
+//         console.log(result);
+//   });
 });
