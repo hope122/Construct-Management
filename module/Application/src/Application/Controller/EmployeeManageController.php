cc0a47b4597b3e0d85d4c013fbfbbffdb6ae30d7
Michael <ms800423@gmail.com>
2015-11-19 16:42:10 +0800

    新增功能初步OK，編輯功能撰寫中

--- module/Application/src/Application/Controller/EmployeeManageController.php
+++ module/Application/src/Application/Controller/EmployeeManageController.php
@@ -58,12 +58,10 @@ class EmployeemanageController extends AbstractActionController
 						
 						$trs .= $tr;
 					}
+					$html = str_replace("@@content@@", $trs, $html);
 				}else{
-					echo "No data";
+					$html = str_replace("@@content@@", $arr->msg, $html);
 				}
-				
-				
-				$html = str_replace("@@content@@", $trs, $html);
 				
 				$pageContent = $html;
 			}
@@ -91,10 +89,60 @@ class EmployeemanageController extends AbstractActionController
 					//echo $action;
 					switch($action){
 						case "insertData":
-							$pageContent = "Prepare to new data";
+							$newPagePath = dirname(__DIR__) . "\\..\\..\\..\\..\\public\\include\\pageSetting\\employeemanage\\newPage.html";
+							$newPage = $VTs->GetHtmlContent($newPagePath);
+							$newPage = str_replace("@@userName@@",$_SESSION["userName"],$newPage);
+							
+							$newPage = str_replace("@@name@@","",$newPage);
+							$newPage = str_replace("@@sid@@","",$newPage);
+							$newPage = str_replace("@@birthday@@","",$newPage);
+							$newPage = str_replace("@@zip@@","",$newPage);
+							$newPage = str_replace("@@city@@","",$newPage);
+							$newPage = str_replace("@@area@@","",$newPage);
+							$newPage = str_replace("@@vil@@","",$newPage);
+							$newPage = str_replace("@@verge@@","",$newPage);
+							$newPage = str_replace("@@road@@","",$newPage);
+							$newPage = str_replace("@@addr@@","",$newPage);
+							$newPage = str_replace("@@mobile@@","",$newPage);
+							$newPage = str_replace("@@tel_h@@","",$newPage);
+							$newPage = str_replace("@@email@@","",$newPage);
+							$newPage = str_replace("@@action@@",$action,$newPage);
+							
+							$pageContent = $newPage;
 							break;
 						case "updateData":
-							$pageContent = "Prepare to update data";
+							$uid = $_POST["uid"];
+							//echo "uid: ".$uid;
+							
+							//$apurl = "http://211.21.170.18:99";
+							$apurl = "http://127.0.0.1:99";
+							$arr = $VTs->json2data($VTs->UrlDataGet($apurl."/employeemanage/getdata?uid=".$uid.""));
+							if( $arr->status ){
+								$editPagePath = dirname(__DIR__) . "\\..\\..\\..\\..\\public\\include\\pageSetting\\employeemanage\\newPage.html";
+								$editPage = $VTs->GetHtmlContent($editPagePath);
+								$editPage = str_replace("@@userName@@",$_SESSION["userName"],$editPage);
+								
+								$editPage = str_replace("@@name@@", $arr->dataList[0]->name, $editPage);
+								$editPage = str_replace("@@sid@@", $arr->dataList[0]->sid, $editPage);
+								$editPage = str_replace("@@birthday@@", $arr->dataList[0]->birthday, $editPage);
+								$editPage = str_replace("@@zip@@", $arr->dataList[0]->zip, $editPage);
+								$editPage = str_replace("@@city@@", $arr->dataList[0]->city, $editPage);
+								$editPage = str_replace("@@area@@", $arr->dataList[0]->area, $editPage);
+								$editPage = str_replace("@@vil@@", $arr->dataList[0]->vil, $editPage);
+								$editPage = str_replace("@@verge@@", $arr->dataList[0]->verge, $editPage);
+								$editPage = str_replace("@@road@@", $arr->dataList[0]->road, $editPage);
+								$editPage = str_replace("@@addr@@", $arr->dataList[0]->addr, $editPage);
+								$editPage = str_replace("@@mobile@@", $arr->dataList[0]->mobile, $editPage);
+								$editPage = str_replace("@@tel_h@@", $arr->dataList[0]->tel_h, $editPage);
+								$editPage = str_replace("@@email@@", $arr->dataList[0]->email, $editPage);
+								$editPage = str_replace("@@action@@",$action,$editPage);
+								
+								$pageContent = $editPage;
+							}else{
+								$pageContent = "Error.";
+							}
+							
+							
 							break;
 						default:
 							$pageContent = "There has errors";
--- public/include/js/page/employeeManage.js
+++ public/include/js/page/employeeManage.js
@@ -1,13 +1,67 @@
-var myRedirect = function(redirectUrl, arg, value) {
-  var form = $('<form action="' + redirectUrl + '" method="post">' +
-  '<input type="hidden" name="'+ arg +'" value="' + value + '"></input>' + '</form>');
-  $('body').append(form);
-  $(form).submit();
+var myRedirect = function(redirectUrl, arg, value, uid) {
+	var form;
+	if(uid != ""){
+		form = $('<form action="' + redirectUrl + '" method="post">' +
+			'<input type="hidden" name="'+ arg +'" value="' + value + '"></input>' + 
+			'<input type="hidden" name="uid" value="' + uid + '"></input>' + '</form>');
+	}else{
+		form = $('<form action="' + redirectUrl + '" method="post">' +
+			'<input type="hidden" name="'+ arg +'" value="' + value + '"></input>' + '</form>');
+	}
+	$('body').append(form);
+	$(form).submit();
 };
 
-function edit(action){
-	//alert(action);
+function edit(action, uid){
+	//alert(uid);
 	var directurl = "http://127.0.0.1:200/employeemanage/editpage";
-	myRedirect(directurl, "action", action);
+	myRedirect(directurl, "action", action, uid);
 }
 
+function checkSubmit(action,uid){
+	var actionUrl;
+	switch(action){
+		case "insertData":
+			actionUrl = "http://127.0.0.1:99/employeemanage/insertdata";
+			break;
+		case "updateData":
+			actionUrl = "http://127.0.0.1:99/employeemanage/updatedata?uid="+uid;
+			break;
+	}
+	
+	//console.log($("#data").serialize());
+	var dataArray = getData(decodeURIComponent($("#data").serialize(),true));
+	$.ajax({
+		//url: configObject.SARRecordAttendance,
+		url: actionUrl,
+        type: "POST",
+		data: { name: dataArray[0][1], sid: dataArray[1][1], sex: dataArray[2][1], birthday: dataArray[3][1],
+				zip: dataArray[4][1], city: dataArray[5][1], area: dataArray[6][1], vil: dataArray[7][1], verge: dataArray[8][1],
+				road: dataArray[9][1], addr: dataArray[10][1],
+				mobile: dataArray[11][1], tel_h: dataArray[12][1], email: dataArray[13][1] },
+		dataType: "JSON",
+		async:false,
+        success: 
+			function(rs){
+				window.location.href = "http://127.0.0.1:200/employeemanage";
+			},
+        error: 
+			function(e){
+				console.log(e);
+			}
+	});
+}
+
+function getData(ori_arr){	
+	var arr = ori_arr.split("&");
+	//console.log(arr[0]);
+	
+	var arr2 = new Array();
+	for(var index in arr){
+		//console.log(arr[index]);
+		arr2[index] =arr[index].split("=");
+	}
+	//console.log(arr2);
+	
+	return arr2;
+}
