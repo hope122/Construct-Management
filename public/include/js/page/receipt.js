38250449364f09faecbc85ce8d19df875733c639
Michael <ms800423@gmail.com>
2015-12-04 17:42:07 +0800

    畫面表格修改

--- public/include/js/page/receipt.js
+++ public/include/js/page/receipt.js
@@ -43,31 +43,36 @@ $(function(){
       }
   });
 
-  //顯示廠商相關資料
+  //顯示相關資料
   $("#supply").change(function(){
     //console.log($(this).val());
-    $.ajax({
-      url: configObject.ReceiptGetData,
-      type: "POST",
-      data: { type: "supplyInfo", suid: $(this).val() },
-      dataType: "JSON",
-      success:
-        function(rs){
-          //console.log(rs);
-          if(rs.status){
-            //呈現資料
-            setView(rs.data);
-            $("#submit").css("visibility","visible");
-          }else{
-            console.log(rs.msg);
-            clearView();
+    if($(this).val()!=0){
+      $.ajax({
+        url: configObject.ReceiptGetData,
+        type: "POST",
+        data: { type: "detial", suid: $(this).val() },
+        dataType: "JSON",
+        success:
+          function(rs){
+            console.log(rs.data.price[0].price);
+            if(rs.status){
+              //呈現資料
+              setView(rs.data);
+              
+            }else{
+              console.log(rs.msg);
+              clearView();
+            }
+          },
+        error:
+          function(e){
+            console.log(e);
           }
-        },
-      error:
-        function(e){
-          console.log(e);
-        }
-    });
+      });
+    }else{
+      clearView();
+      $("#receipt").hide();
+    }
   });
 });
 
@@ -87,49 +92,118 @@ function createSelection($dataArr,$id){
 }
 
 function setView($dataArr){
-   console.log($dataArr[0]);
+  //console.log($dataArr);
+  if(!$.isEmptyObject($dataArr.supplyInfo) && !$.isEmptyObject($dataArr.dateRange) && !$.isEmptyObject($dataArr.price) && !$.isEmptyObject($dataArr.detial)){
+    //廠商資料  
+    setSupplyInfo($dataArr.supplyInfo);
 
-  //廠商資料
-  $("#su_name").text($dataArr[0].su_name);
-  $("#owner").text($dataArr[0].owner);
-  $address = getAddress($dataArr);
-  // console.log($address);
-  $("#su_address").text($address);
-  $("#su_tel_o").text($dataArr[0].tel_o);
-  $("#mobile").text($dataArr[0].mobile);
+    //付款資料
+    setPaymentData($dataArr.dateRange[0], $dataArr.price[0]);
 
-  //付款資料
-  $("#voucher").text($dataArr[0].voucher);
-  $("#dates").text($dataArr[0].dates);
-  $("#datee").text($dataArr[0].datee);
-  $("#price").text($dataArr[0].price);
-  $("#pay_date").text($dataArr[0].pay_date);
+    //明細資料
+    setList($dataArr.detial);
 
-  setList($dataArr);
+    $("#receipt").show();
+    $("#submit").css("visibility","visible");
+  }else{
+    //清除資料
+    clearView();
+    $("#receipt").hide();
+    $("#submit").css("visibility","hidden");
+
+    alert("無資料");
+  }
 }
 
 function getAddress($dataArr){
-  return $dataArr[0].zip + $dataArr[0].city + $dataArr[0].area +
-         $dataArr[0].vil + $dataArr[0].verge + $dataArr[0].road + $dataArr[0].addr;
+  //console.log($dataArr);
+  $.each($dataArr,function(i,v){
+    if(typeof(v)!="string"){
+      $dataArr[i]="";
+    }
+  });
+  //console.log($dataArr);
+  return $dataArr.zip + $dataArr.city + $dataArr.area +
+         $dataArr.vil + $dataArr.verge + $dataArr.road + $dataArr.addr;
 }
 
 function clearView(){
+  //廠商
   $("#su_name").text("");
   $("#owner").text("");
-  $("#su_address").text("");
-  $("#su_tel_o").text("");
+  $("#zip").text("");
+  $("#city").text("");
+  $("#area").text("");
+  $("#vil").text("");
+  $("#verge").text("");
+  $("#road").text("");
+  $("#addr").text("");
+  $("#tel_o").text("");
   $("#mobile").text("");
 
   //付款資料
-  $("#voucher").text("");
   $("#dates").text("");
   $("#datee").text("");
-  $("#amount").text("");
-  $("#pay_date").text("");
+  $("#price").text("");
+
+  //明細
+  $("tr[name='newTr']").remove();
 }
 
 function setList($dataArr){
-  $.each($dataArr,function(i,v){
-    console.log(i,v);
+  //console.log($dataArr);
+  if(!$.isEmptyObject($dataArr)){
+    $.each($dataArr,function(i,v){
+      //console.log(i,v);
+      var $tr = $("<tr/>").attr("name","newTr");
+      $("<td/>").attr("width","16%").text(v.n1).appendTo($tr);
+      $("<td/>").attr("width","16%").text(v.n2).appendTo($tr);
+      $("<td/>").attr("width","16%").text(v.n3).appendTo($tr);
+      $("<td/>").attr("width","16%").text(v.n4).appendTo($tr);
+      $("<td/>").attr("width","16%").text(v.qty).appendTo($tr);
+      $("<td/>").attr("width","16%").text(v.unit1).appendTo($tr);
+      $tr.appendTo($("#payment"));
+    });
+  }else{
+    $("tr[name='newTr']").remove();
+  }
+}
+
+function setSupplyInfo($dataArr){
+  if(!$.isEmptyObject($dataArr)){
+    $.each($dataArr[0],function(i,v){
+      if(typeof(v)=="string"){
+        $("#"+i).text(v);
+      }
+    });
+  }
+}
+
+function setPaymentData($dateRange, $price){
+  $.each($dateRange,function(i,v){
+    if(typeof(v)=="string"){
+      $("#"+i).text(v);
+    }else{
+      $("#"+i).text("");
+    }
   });
+  $.each($price,function(i,v){
+    if(typeof(v)=="string"){
+      $("#"+i).text(parsePrice(v));
+    }else{
+      $("#"+i).text("");
+    }
+  });
+}
+
+function parsePrice($price){
+  var str = $price;
+  for(var i=1, j=0; i<=Math.floor($price.length/3); i++,j++){
+    str = str.substr(0,str.length-3*i-j) + "," + str.substr(str.length-3*i-j, str.length);
+  }
+  if($price.length%3==0){
+    str = str.substr(1,str.length);
+  }
+
+  return str;
 }
