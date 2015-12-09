50ab4c41e297b36972c5a7f8631e40d215dde83d
DelphiRace <delphi.race@gmail.com>
2015-12-09 17:06:45 +0800

    add dailyconstruction

---
+++ public/include/js/page/dailyconstruction.js
@@ -0,0 +1,84 @@
+$(function(){
+  //取得列表
+  getDailyList("floor");
+});
+
+function getDailyList(type){
+    var formOption = $("#menuSelect").serialize();
+    formOption += "&puid=1&type="+type;
+    //取得列表
+    $.ajax({
+       url: configObject.getDailyContent,
+       type: "POST",
+       data: formOption,
+       dataType: "JSON",
+       success: function(rs){
+           $("#worklist").empty();
+           if(rs.status){
+              switch(type){
+                case "floor":
+                  var options = $("#floorid");
+                break;
+                case "workitem":
+                  var options = $("#workitemid");
+                break;
+                case "pacemodel":
+                  var options = $("#pacemodelid");
+                break;
+                case "area":
+                  var options = $("#areaid");
+                break;
+                case "alllist":
+                  var options = $("#alllist");
+                break;
+              }
+
+              if(type != "alllist"){
+                var resetOptionStr = '<option value="0">請選擇</option>',resetStr = "";
+                options.empty().append(resetOptionStr);
+                $.each(rs.daily,function(i,v){
+                    $('<option value="'+v.uid+'">'+v.Name+'</p>').appendTo(options);
+                });
+              }else if(type == "alllist"){
+                var strContent = "";
+                if(rs.daily != null){
+                  $.each(rs.daily,function(i,v){
+                        strContent += '<ul>';
+                        strContent += '<li><label>';
+                        strContent += checkAction(v.uid);
+                        strContent += '<h3>'+v.Name+'</h3>';
+                        strContent += '</label></li>';
+                        strContent += '</ul>';
+                  });
+                }
+                options.empty().html(strContent);
+              }
+           }else{
+               //navigator.notification.alert(rs.msg,null,"Error");
+           }
+       }
+   });
+}
+
+function checkAction(id){
+  return '<div class="checkitem"><input type="checkbox" name="alllistid[]" value="'+id+'"></div>';
+    
+}
+
+function checkItems(){
+  var formOption = $("#alllist").serialize();
+  formOption += "&type=checkData";
+  $.ajax({
+       url: configObject.getDailyContent,
+       type: "POST",
+       data: formOption,
+       //dataType: "JSON",
+       success: function(rs){
+        getDailyList('alllist');
+      }
+  });
+}
+
+function backWorkList(){
+    location.replace("work_project.html");
+}
