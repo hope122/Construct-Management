1233f67c52ca2628bf8c036c2485864d294c5c29
DelphiRace <delphi.race@gmail.com>
2015-12-21 17:13:50 +0800

    update

--- public/include/js/config/config.js
+++ public/include/js/config/config.js
@@ -37,5 +37,5 @@ var configObject = {
     "engDeleteData": apurl+"/engineeringmanage/deletedata",
     "engInsertData": apurl+"/engineeringmanage/insertdata",
     "engUpdateData": apurl+"/engineeringmanage/updatedata",
-    "chartsWS": originUrl+":8080"
+    "chartsWS": location.origin+":8080"
 };
