7f58a4e0843de48f0851340d6b639b220d0ffde9
DelphiRace <delphi.race@gmail.com>
2016-01-15 17:44:44 +0800

    update

--- public/include/js/contentpages/dispositif/dispositif.js
+++ public/include/js/contentpages/dispositif/dispositif.js
@@ -56,7 +56,8 @@ $.datepicker._generateMonthYearHeader = function(inst, drawMonth, drawYear, minD
 
 function getInput(){
 	var sendData = getUsetInput();
-	//sendRequest("get",sendUrl,sendData,{},"xml","getResponses");
+	var sendUrl = "http://211.21.170.17:8080/wsContract/Main.asmx/SetMain";
+	sendRequest("post",sendUrl,sendData,"json","xml","getResponses");
 	console.log(sendData);
 }
 
