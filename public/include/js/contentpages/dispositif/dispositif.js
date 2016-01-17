7714b07da8f12674f1ae51731c87bcacdf26525d
DelphiRace <delphi.race@gmail.com>
2016-01-17 19:54:15 +0800

    update

--- public/include/js/contentpages/dispositif/dispositif.js
+++ public/include/js/contentpages/dispositif/dispositif.js
@@ -58,7 +58,7 @@ function getInput(){
 	// var sendData = {};
 	// sendData.sData = getUsetInput();
 	// sendData = $.param(sendData);
-	var sendData = "sData="+JSON.parse(getUsetInput());
+	var sendData = "sData="+JSON.stringify(getUsetInput());
 	var sendUrl = "http://211.21.170.17:8080/wsContract/Main.asmx/SetMain";
 	// sendRequest("post",sendUrl,sendData,"json","xml","getResponses");
 	sendRequest("post",sendUrl,sendData,"","xml","getResponses");
