fd746df161d8d5268def2d03119a9638760241c1
DelphiRace <delphi.race@gmail.com>
2016-04-12 17:05:02 +0800

    update api url

--- public/include/js/contentpages/organize_job/config.js
+++ public/include/js/contentpages/organize_job/config.js
@@ -1,3 +1,3 @@
-var ASS_API = configObject.WebAPI + "/ASS/api/";
+var ASS_API = configObject.WebAPI + "/v201604/ASS/api/";
 var ctrlAdminAPI = ASS_API + "ctrlAdmin/";
-var ctrlAdminDelAPI = "ASS/api/ctrlAdmin/";
+var ctrlAdminDelAPI = "v201604/ASS/api/ctrlAdmin/";
--- public/include/js/contentpages/person/config.js
+++ public/include/js/contentpages/person/config.js
@@ -1,3 +1,3 @@
-var ASS_API = configObject.WebAPI + "/ASS/api/";
+var ASS_API = configObject.WebAPI + "/v201604/ASS/api/";
 var ctrlPersonAPI = ASS_API + "ctrlPerson/";
-var ctrlPersonDelAPI = "ASS/api/ctrlPerson/";
+var ctrlPersonDelAPI = "v201604/ASS/api/ctrlPerson/";
