99a9eacc43a069a350467cb551a64d4f17199235
DelphiRace <delphi.race@gmail.com>
2016-06-15 05:06:01 +0800

    update

--- public/include/js/main/userMenu.js
+++ public/include/js/main/userMenu.js
@@ -1,31 +1,24 @@
 function getMenus(loginInfo){
-  var menus = porcessData(configObject.getmenu,loginInfo);
-  console.log(menus);
-
+  // var menus = porcessData(configObject.getmenu,loginInfo);
 	var menus = porcessData(configObject.getUserMenu,{});
-	console.log(menus);
   var options = {
       idName: "uid",
       title: "memo",
   }
 
   meunData = processMenuTreeDataOnly(menus.data,options);
-  console.log(meunData);
   $("#menus").empty();
   $.each(meunData,function(i, content){
-    // console.log(content);
+    content.find("a").click(function(){
+      var thisHref = $(this).attr("href");
+      if(thisHref != "#"){
+        $("#pagescontent").empty();
+        loadPage($(this).attr("href"),"pagescontent");
+      }
+      return false;
+   });
     content.appendTo($("#menus"));
   });
-	// $("#menus").html(menus.menu)
-	// .find("a").click(function(){
- //      var thisHref = $(this).attr("href");
- //      if(thisHref != "#"){
- //        //location.href = location.origin + "/" + $(this).attr("href");
- //        $("#pagescontent").empty();
- //        loadPage($(this).attr("href"),"pagescontent");
- //      }
- //      return false;
- //  	});
  //  	getLan();
 	loadJS("include/strongly/assets/js/content-main.js","keep");
 }
