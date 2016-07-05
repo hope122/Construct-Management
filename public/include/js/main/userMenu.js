8fd9633ece01c8a4ed1583c71fe3edd888599642
DelphiRace <delphi.race@gmail.com>
2016-07-05 15:35:37 +0800

    update

--- public/include/js/main/userMenu.js
+++ public/include/js/main/userMenu.js
@@ -7,20 +7,21 @@ function getMenus(loginInfo){
   }
 
   meunData = processMenuTreeDataOnly(menus.data,options);
+  // console.log(meunData);
   $("#menus").empty();
   $.each(meunData,function(i, content){
-    content.find("a").click(function(){
-      var thisHref = $(this).attr("href");
-      if(thisHref != "#"){
-        $("#pagescontent").empty();
-        loadPage($(this).attr("href"),"pagescontent");
-      }
-      // 登出機制，只要再CLASS裡面有logout關鍵字的連結走這裡
-      if($(this).prop("class").search("logout") != -1){
-        logoutEven();
-      }
-      return false;
-    });
+    // content.find("a").click(function(){
+    //   var thisHref = $(this).attr("href");
+    //   if(thisHref != "#"){
+    //     $("#pagescontent").empty();
+    //     loadPage($(this).attr("href"),"pagescontent");
+    //   }
+    //   // 登出機制，只要再CLASS裡面有logout關鍵字的連結走這裡
+    //   if($(this).prop("class").search("logout") != -1){
+    //     logoutEven();
+    //   }
+    //   return false;
+    // });
     content.appendTo($("#menus"));
   });
  //  	getLan();
