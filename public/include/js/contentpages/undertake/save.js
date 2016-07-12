441ee545ef09877ad2d966bce4022f8abf12b24a
hope122 <hope080122@gmail.com>
2016-07-12 11:50:19 +0800

    Reference
    main.js
    save.js

--- public/include/js/contentpages/undertake/save.js
+++ public/include/js/contentpages/undertake/save.js
@@ -1,33 +1,48 @@
 // 儲存收文新增/修改
 function saveReferenceData(sendObj, modifyItem, putFormArea){
     // return
-    sendObj.typeID = 1;
-    sendObj.userName = userLoginInfo.userName;
-    var sendData = {
+    sendObj.typeId = 5;
+    //sendObj.userName = userLoginInfo.userName;
+	sendObj.levelId= 1;
+	sendObj.isopycnicId=1;
+	sendObj.userName="test1";
+	// sendObj.uid=16;
+	
+	var webapi="setReferenceInsert";
+	var url=wrsUrl;
+	if($(putFormArea).find("input:file").length>0){
+		url=wrsAPI + "uploaderAPI";
+		webapi="setReferenceDocInsert"
+	}
+	var sendData = {
         // api: docAPI+"setDocFileInsert",
-        api: referenceAPI+"setReferenceInsert",
+        api: referenceAPI+webapi,
         data: sendObj
     };
-    console.log(sendObj);
+     // console.log(sendData);
 
-    $.post(wrsUrl, sendData, function(rs){
-        console.log(rs);
-    });
-    // var options = {
-    //     url: wrsAPI+"uploaderAPI",
-    //     type:"POST",
-    //     data: sendData,
-    //     dataType:"JSON",
-    //     beforesend: function(xhr){
-    //         // testBs3Show(xhr);
-    //     },
-    //     uploadProgress: function(event, position, total, percentComplete) {
-    //        console.log(event, position, total, percentComplete);
+    // $.post(wrsUrl, sendData, function(rs){
+        // console.log(rs);
+    // });
+	
+    var options = {
+		//url:wrsAPI + "uploaderAPI",
+        // url: wrsUrl,
+		url:url,
+        type:"POST",
+        data: sendData,
+        dataType:"JSON",
+        beforesend: function(xhr){
+            // testBs3Show(xhr);
+        },
+        uploadProgress: function(event, position, total, percentComplete) {
+          // console.log(event, position, total, percentComplete);
 
-    //     },
-    //     success: function(rs) {
-    //        console.log(rs);
-    //     },
-    // };
-    // $(putFormArea).ajaxSubmit(options);
+        },
+        success: function(rs) {
+           console.log(rs);
+        },
+    };
+    $(putFormArea).ajaxSubmit(options);
+
 }
