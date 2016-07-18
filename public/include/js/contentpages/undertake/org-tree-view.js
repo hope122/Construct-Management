4cfc9936776c4bca06648e5f31d2a19e8181f3ee
DelphiRace <delphi.race@gmail.com>
2016-07-18 13:48:23 +0800

    update

--- public/include/js/contentpages/undertake/org-tree-view.js
+++ public/include/js/contentpages/undertake/org-tree-view.js
@@ -65,7 +65,7 @@ function userListDialog(data ,uid){
             valeTag: "uid",
             button:[
                 {
-                    text: "返回",
+                    text: "取消",
                     // className: "btn-success",
                     click: function(){
                     
@@ -74,7 +74,7 @@ function userListDialog(data ,uid){
                     }
                 },
                 {
-                    text: "確定",
+                    text: "下一步",
                     className: "btn-success",
                     click: function(){
                         var userIDList = userListDialog.getValue();
@@ -83,8 +83,8 @@ function userListDialog(data ,uid){
                         sendData.uid = uid;
                         console.log(sendData);
                         if(userIDList){
-                            
-                            $("#userListDialog").bsDialog("close");
+                            setReferenceDateDailog(sendData);
+                            // $("#userListDialog").bsDialog("close");
                         }else{
                             
                         }
@@ -109,4 +109,71 @@ function saveOffice(uid,officeId){
     });
 
 
+}
+
+// 設定預警日期
+function setReferenceDateDailog(sendData){
+    $("#setReferenceDateDailog").remove();
+    $("<div>").prop("id","setReferenceDateDailog").appendTo("body");
+
+    $("#setReferenceDateDailog").bsDialog({
+        autoShow:true,
+        showFooterBtn:true,
+        // headerCloseBtn:false,
+        // modalClass: "bsDialogWindow",
+        title: "設置預警日期",
+        start: function(){
+            var option = {
+                styleKind: "received-issued",style:"undertake-dateandtype"
+            }
+            getStyle(option,function(pageStyle){
+                var pageStyleObj = $.parseHTML(pageStyle);
+
+                $(pageStyleObj).find(".list-items").eq(0).find(".control-label").eq(0).text("設置預警日期");
+                var dateOption = {
+                    dateFormat: "yy-mm-dd",
+                    
+                    showOn: "button",
+                    buttonText: '<i class="fa fa-calendar fa-lg mouse-pointer send-btn"></i>',
+                    onSelect: function(dateText, inst) {
+                        // end_date_content
+                        $(pageStyleObj).find("#end_date_content").removeClass("item-bg-danger").text(dateText);
+
+                    },
+                    minDate: 0
+                }
+
+                $(pageStyleObj).find("#end_date").datepicker(dateOption);
+                
+                $(pageStyleObj).appendTo($("#setReferenceDateDailog").find(".modal-body"));
+            });
+        },
+        button:[
+            {
+                text: "返回",
+                // className: "btn-success",
+                click: function(){
+                
+                    $("#setReferenceDateDailog").bsDialog("close");
+                    
+                }
+            },
+            {
+                text: "設定",
+                className: "btn-success",
+                click: function(){
+                    var end_date = $("#setReferenceDateDailog").find("#end_date").val();
+                    sendData.end_date = end_date;
+                    console.log(sendData);
+                    if(end_date){
+                        
+
+                    }else{
+                        $("#setReferenceDateDailog").find("#end_date_content").text("尚未選擇日期").addClass("item-bg-danger");
+                    }
+                }
+            }
+
+        ]
+    });
 }
